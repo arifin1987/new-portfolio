7431dbf216034a6101edf1f17224da30e21a8381
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-06-02 06:46:24 -0700

    Banner added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -3,16 +3,27 @@ import Container from "../Container";
 
 const Navbar = () => {
   return (
-    <div className="fixed w-full bg-white z-10 shadow-sm">
+    <div className=" w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex items-center justify-between gap-3 md:gap-0">
             <div>
-              <Link>Shohidul Arifin</Link>
+              <Link className="text-2xl ">
+                {" "}
+                <span className="text-emerald-600">Shohidul</span>{" "}
+                <span className="text-blue-600">Arifin</span>
+              </Link>
             </div>
-            <div className="space-x-4">
-              <Link to="/">Home</Link>
-              <Link to="/dashboard">Dashboard</Link>
+            <div className="space-x-8">
+              <Link className="text-blue-500 hover:text-red-500" to="/">
+                Home
+              </Link>
+              <Link
+                className="text-blue-500 hover:text-red-500"
+                to="/dashboard"
+              >
+                Dashboard
+              </Link>
             </div>
           </div>
         </Container>
---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,61 @@
+import { FaDownload, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import Container from "../../../components/Shared/Container";
+import { Rotate, Slide } from "react-awesome-reveal";
+
+import img1 from "../../../assets/SA CV PIC.jpg";
+
+const Banner = () => {
+  return (
+    <Container>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={img1}
+            width="500"
+            height="500"
+            className="max-w-sm rounded-[50%] shadow-2xl"
+          />
+          <div>
+            <Slide>
+              <h1 className="text-5xl font-bold py-4 text-emerald-500">
+                Hi, I am Shohidul Arifin
+              </h1>
+            </Slide>
+            <Rotate>
+              <h2 className="text-3xl py-2 ">MERN Stack Developer</h2>
+            </Rotate>
+
+            <div className="flex gap-8 my-2 text-xl">
+              <div>
+                <a target="_blank" href="https://github.com/arifin1987">
+                  Github<FaGithub></FaGithub>
+                </a>
+              </div>
+              <div className="text-sky-500">
+                <a
+                  target="_blank"
+                  href="https://www.linkedin.com/in/shohidul-arifin-257527237"
+                >
+                  LinkedIn<FaLinkedinIn></FaLinkedinIn>
+                </a>
+              </div>
+            </div>
+            <h3 className="py-6  text-xl text-emerald-500 w-[70%]">
+              I have vast experience in TypeScript, NodeJS, ExpressJS, ReactJS,
+              Next.js Redux Tool Kit, MongoDB, Firebase, Bootstrap, Tailwind
+              css, Ant Design, Framer Motion etc.
+            </h3>
+            <button className="btn btn-primary">
+              {" "}
+              <a href="Shohidul Arifin_MERN-Stack_CV.pdf" download>
+                Download Resume <FaDownload></FaDownload>
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Banner;
